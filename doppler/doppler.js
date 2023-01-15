5464713b6a6bfad88e876c8f4c9bb3719170cc30
Elias Dobrin <elias@dobrin.net>
2023-01-14 17:39:27 -0800

    Added recenter button and fixed bugs

--- doppler/doppler.js
+++ doppler/doppler.js
@@ -1,7 +1,7 @@
-var up = false;
-var down = false;
-var left = false;
-var right = false;
+let up = false;
+let down = false;
+let left = false;
+let right = false;
 
 function goUp()
 {
@@ -35,8 +35,14 @@ function goStop()
   right = false;
 }
 
-var time = 0;
-var lightbulb =
+function goCenter()
+{
+  source.position.x = 0
+  source.position.y = 0
+}
+
+let time = 0;
+const source =
 {
   wavelength: 550,
   position:
@@ -45,7 +51,7 @@ var lightbulb =
     y: 0
   }
 };
-var observer =
+const observer =
 {
   lastWaveTime: 0,
   position:
@@ -54,33 +60,33 @@ var observer =
     y: 150
   }
 };
-var waves = [];
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+const waves = [];
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
 function render()
 {
-  var speed = Number(document.getElementById("speed-input").value);
+  let speed = Number(document.getElementById("speed-input").value);
   document.getElementById("speed-value").innerHTML = "Speed of the source is: " + speed + " time(s) the speed of sound";
 
   if(up)
   {
-    lightbulb.position.y -= speed;
+    source.position.y -= speed;
   }
   
   if(down)
   {
-    lightbulb.position.y += speed;
+    source.position.y += speed;
   }
   
   if(left)
   {
-    lightbulb.position.x -= speed;
+    source.position.x -= speed;
   }
   
   if(right)
   {
-    lightbulb.position.x += speed;
+    source.position.x += speed;
   }
 
   if(time % 10 === 0)
@@ -91,8 +97,8 @@ function render()
       seen: false,
       position:
       {
-        x: lightbulb.position.x,
-        y: lightbulb.position.y
+        x: source.position.x,
+        y: source.position.y
       }
     });
   }
@@ -102,18 +108,18 @@ function render()
   
   ctx.translate(250, 250);
   
-  var hue = 280 * ((300 - (lightbulb.wavelength - 400)) / (700 - 400));
+  let hue = 280 * ((300 - (source.wavelength - 400)) / (700 - 400));
   ctx.fillStyle = "hsl(" + hue + ", 100%, 50%)";
   ctx.beginPath();
-  ctx.arc(lightbulb.position.x, lightbulb.position.y, 20, 0, 2 * Math.PI);
+  ctx.arc(source.position.x, source.position.y, 20, 0, 2 * Math.PI);
   ctx.fill();
   
-  ctx.fillText("SRC", lightbulb.position.x - 4, lightbulb.position.y + 30);
+  ctx.fillText("SRC", source.position.x - 4, source.position.y + 30);
   
   if(observer.timeDifference !== undefined)
   {
-    var newWavelength = Math.max(Math.min(lightbulb.wavelength * observer.timeDifference / 10, 700), 400);
-    var newHue = 280 * ((300 - (newWavelength - 400)) / (700 - 400));
+    let newWavelength = Math.max(Math.min(source.wavelength * observer.timeDifference / 10, 700), 400);
+    let newHue = 280 * ((300 - (newWavelength - 400)) / (700 - 400));
     ctx.fillStyle = "hsl(" + newHue + ", 100%, 50%)";
   }
   
@@ -131,9 +137,9 @@ function render()
   ctx.strokeStyle = "white";
   ctx.lineWidth = 2;
   
-  for(var n = 0; n < waves.length; n++)
+  for(let n = 0; n < waves.length; n++)
   {
-    var wave = waves[n];
+    const wave = waves[n];
     ctx.beginPath();
     ctx.arc(wave.position.x, wave.position.y, wave.time, 0, 2 * Math.PI);
     ctx.stroke();
