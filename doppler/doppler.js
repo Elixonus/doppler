c080a212dffed7d7226e7ae9fcc3e2f310bd8f62
Elias Dobrin <elias@dobrin.net>
2023-01-29 18:56:58 -0800

    added containers for wave frequency and amplitude and added javascript (plenty)

--- doppler/doppler.js
+++ doppler/doppler.js
@@ -1,17 +1,108 @@
-document.getElementById("time-strt").onclick = function(){};
-document.getElementById("time-stop").onclick = function(){};
-document.getElementById("bufr-save").onclick = function(){};
-document.getElementById("bufr-rest").onclick = function(){};
-document.getElementById("pwr-on").onclick = function(){};
-document.getElementById("pwr-off").onclick = function(){};
-document.getElementById("type-pos").onclick = function(){};
-document.getElementById("type-vel").onclick = function(){};
-document.getElementById("type-acc").onclick = function(){};
-document.getElementById("dir-left").onclick = function(){};
-document.getElementById("dir-rght").onclick = function(){};
-document.getElementById("dir-up").onclick = function(){};
-document.getElementById("dir-down").onclick = function(){};
-document.getElementById("dir-zero").onclick = function(){};
-document.getElementById("mag-low").onclick = function(){};
-document.getElementById("mag-med").onclick = function(){};
-document.getElementById("mag-high").onclick = function(){};
+function timeStrt()
+{
+    run = true;
+}
+
+function timeStop()
+{
+    run = false;
+}
+
+function ctrlSrc()
+{
+    obs.ctrl = false;
+    src.ctrl = true;
+}
+
+function ctrlObs()
+{
+    src.ctrl = false;
+    obs.ctrl = true;
+}
+
+function pwrOn()
+{
+    if(src.ctrl)
+    {
+        src.pwr = true;
+    }
+
+    if(obs.ctrl)
+    {
+        obs.pwr = true;
+    }
+}
+
+function pwrOff()
+{
+    if(src.ctrl)
+    {
+        src.pwr = false;
+    }
+
+    if(obs.ctrl)
+    {
+        obs.pwr = false;
+    }
+}
+
+document.getElementById("time-strt").onclick = timeStrt();
+document.getElementById("time-stop").onclick = timeStop();
+document.getElementById("bufr-save").onclick = bufrSave();
+document.getElementById("bufr-rstr").onclick = bufrRstr();
+document.getElementById("ctrl-src").onclick = ctrlSrc();
+document.getElementById("ctrl-obs").onclick = ctrlObs();
+document.getElementById("pwr-on").onclick = pwrOn();
+document.getElementById("pwr-off").onclick = pwrOff();
+document.getElementById("type-pos").onclick = typePos();
+document.getElementById("type-vel").onclick = typeVel();
+document.getElementById("type-acc").onclick = typeAcc();
+document.getElementById("dir-left").onclick = dirLeft();
+document.getElementById("dir-rght").onclick = dirRght();
+document.getElementById("dir-up").onclick = dirUp();
+document.getElementById("dir-down").onclick = dirDown();
+document.getElementById("dir-zero").onclick = dirZero();
+document.getElementById("mag-low").onclick = magLow();
+document.getElementById("mag-med").onclick = magMed();
+document.getElementById("mag-high").onclick = magHigh();
+
+let run = true;
+
+const src = {
+    ctrl: true,
+    pwr: true,
+    freq: 1,
+    amp: 1,
+    pos: {
+        x: 0,
+        y: 0
+    },
+    vel: {
+        x: 0,
+        y: 0
+    },
+    acc: {
+        x: 0,
+        y: 0
+    }
+}
+
+const obs = {
+    ctrl: false,
+    pwr: true,
+    freq: 1,
+    amp: 1,
+    type: 1,
+    pos: {
+        x: 0,
+        y: 0
+    },
+    vel: {
+        x: 0,
+        y: 0
+    },
+    acc: {
+        x: 0,
+        y: 0
+    }
+}
