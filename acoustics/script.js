678e56caaa9710ad7c0a6da292edfce543a9e3f8
Elias Dobrin <elias@dobrin.net>
2025-03-06 14:37:24 -0800

    fixed order of rendering

--- acoustics/script.js
+++ acoustics/script.js
@@ -91,6 +91,10 @@ function doViewGeo() {
     ctxGeo.fillStyle = "#f00";
     ctxGeo.fill();
 
+    let dist;
+    let svel;
+    let ovel;
+
     if (obs.wav !== null) {
         ctxGeo.beginPath();
         ctxGeo.arc(obs.wav.pos.x, obs.wav.pos.y, 0.05, 0, 2 * Math.PI);
@@ -104,61 +108,19 @@ function doViewGeo() {
         ctxGeo.strokeStyle = "#fff";
         ctxGeo.stroke();
 
-        if (Math.hypot(obs.wav.vel.x, obs.wav.vel.y) > 0.01 * wspd) {
-            ctxGeo.save();
-            ctxGeo.translate(obs.wav.pos.x, obs.wav.pos.y);
-            ctxGeo.beginPath();
-            ctxGeo.lineTo(0, 0);
-            ctxGeo.translate(obs.wav.vel.x / wspd, obs.wav.vel.y / wspd);
-            ctxGeo.rotate(Math.atan2(obs.wav.vel.y, obs.wav.vel.x));
-            ctxGeo.lineTo(0, 0);
-            ctxGeo.lineWidth = 0.02;
-            ctxGeo.strokeStyle = "#ff0";
-            ctxGeo.stroke();
-            ctxGeo.beginPath();
-            ctxGeo.lineTo(-0.1, 0.1);
-            ctxGeo.lineTo(-0.1, -0.1);
-            ctxGeo.lineTo(0, 0);
-            ctxGeo.closePath(); 
-            ctxGeo.stroke();
-            ctxGeo.restore();
-        }
-    }
-
-    if (obs.wav !== null) {
-        let dist = Math.hypot(obs.wav.pos.x - obs.pos.x, obs.wav.pos.y - obs.pos.y);
-        let svel = (obs.wav.vel.x * (obs.wav.pos.x - obs.pos.x) + obs.wav.vel.y * (obs.wav.pos.y - obs.pos.y)) / dist;
+        dist = Math.hypot(obs.wav.pos.x - obs.pos.x, obs.wav.pos.y - obs.pos.y);
+        svel = (obs.wav.vel.x * (obs.wav.pos.x - obs.pos.x) + obs.wav.vel.y * (obs.wav.pos.y - obs.pos.y)) / dist;
         svel = {
             x: svel * (obs.wav.pos.x - obs.pos.x) / dist,
             y: svel * (obs.wav.pos.y - obs.pos.y) / dist
         };
-        let ovel = (obs.vel.x * (obs.pos.x - obs.wav.pos.x) + obs.vel.y * (obs.pos.y - obs.wav.pos.y)) / dist;
+        ovel = (obs.vel.x * (obs.pos.x - obs.wav.pos.x) + obs.vel.y * (obs.pos.y - obs.wav.pos.y)) / dist;
         ovel = {
             x: ovel * (obs.pos.x - obs.wav.pos.x) / dist,
             y: ovel * (obs.pos.y - obs.wav.pos.y) / dist
         };
-    
-        if (Math.hypot(svel.x, svel.y) > 0.01 * wspd) {
-            ctxGeo.save();
-            ctxGeo.translate(obs.wav.pos.x, obs.wav.pos.y);
-            ctxGeo.beginPath();
-            ctxGeo.lineTo(0, 0);
-            ctxGeo.translate(svel.x / wspd, svel.y / wspd);
-            ctxGeo.rotate(Math.atan2(svel.y, svel.x));
-            ctxGeo.lineTo(0, 0);
-            ctxGeo.lineWidth = 0.02;
-            ctxGeo.strokeStyle = "#880";
-            ctxGeo.stroke();
-            ctxGeo.beginPath();
-            ctxGeo.lineTo(-0.1, 0.1);
-            ctxGeo.lineTo(-0.1, -0.1);
-            ctxGeo.lineTo(0, 0);
-            ctxGeo.closePath(); 
-            ctxGeo.stroke();
-            ctxGeo.restore();
-        }
 
-        if (Math.hypot(ovel.x, ovel.y) > 0.01 * wspd) {    
+        if (Math.hypot(ovel.x, ovel.y) > 0.01 * wspd) {
             ctxGeo.save();
             ctxGeo.translate(obs.pos.x + ovel.x / wspd, obs.pos.y + ovel.y / wspd);
             ctxGeo.rotate(Math.atan2(ovel.y / wspd, ovel.x / wspd));
@@ -186,27 +148,9 @@ function doViewGeo() {
             ctxGeo.strokeStyle = "#fff";
             ctxGeo.stroke();
             ctxGeo.restore();
-
-            ctxGeo.save();
-            ctxGeo.translate(obs.pos.x, obs.pos.y);
-            ctxGeo.beginPath();
-            ctxGeo.lineTo(0, 0);
-            ctxGeo.translate(ovel.x / wspd, ovel.y / wspd);
-            ctxGeo.rotate(Math.atan2(ovel.y, ovel.x));
-            ctxGeo.lineTo(0, 0);
-            ctxGeo.lineWidth = 0.02;
-            ctxGeo.strokeStyle = "#080";
-            ctxGeo.stroke();
-            ctxGeo.beginPath();
-            ctxGeo.lineTo(-0.1, 0.1);
-            ctxGeo.lineTo(-0.1, -0.1);
-            ctxGeo.lineTo(0, 0);
-            ctxGeo.closePath(); 
-            ctxGeo.stroke();
-            ctxGeo.restore();
         }
 
-        if (Math.hypot(svel.x, svel.y) > 0.01 * wspd) {    
+        if (Math.hypot(svel.x, svel.y) > 0.01 * wspd) {
             ctxGeo.save();
             ctxGeo.translate(obs.wav.pos.x + svel.x / wspd, obs.wav.pos.y + svel.y / wspd);
             ctxGeo.rotate(Math.atan2(svel.y / wspd, svel.x / wspd));
@@ -234,24 +178,6 @@ function doViewGeo() {
             ctxGeo.strokeStyle = "#fff";
             ctxGeo.stroke();
             ctxGeo.restore();
-
-            ctxGeo.save();
-            ctxGeo.translate(obs.wav.pos.x, obs.wav.pos.y);
-            ctxGeo.beginPath();
-            ctxGeo.lineTo(0, 0);
-            ctxGeo.translate(svel.x / wspd, svel.y / wspd);
-            ctxGeo.rotate(Math.atan2(svel.y, svel.x));
-            ctxGeo.lineTo(0, 0);
-            ctxGeo.lineWidth = 0.02;
-            ctxGeo.strokeStyle = "#880";
-            ctxGeo.stroke();
-            ctxGeo.beginPath();
-            ctxGeo.lineTo(-0.1, 0.1);
-            ctxGeo.lineTo(-0.1, -0.1);
-            ctxGeo.lineTo(0, 0);
-            ctxGeo.closePath(); 
-            ctxGeo.stroke();
-            ctxGeo.restore();
         }
     }
 
@@ -273,6 +199,70 @@ function doViewGeo() {
         ctxGeo.closePath(); 
         ctxGeo.stroke();
         ctxGeo.restore();
+    }
+
+    if (obs.wav !== null) {
+        if (Math.hypot(obs.wav.vel.x, obs.wav.vel.y) > 0.01 * wspd) {
+            ctxGeo.save();
+            ctxGeo.translate(obs.wav.pos.x, obs.wav.pos.y);
+            ctxGeo.beginPath();
+            ctxGeo.lineTo(0, 0);
+            ctxGeo.translate(obs.wav.vel.x / wspd, obs.wav.vel.y / wspd);
+            ctxGeo.rotate(Math.atan2(obs.wav.vel.y, obs.wav.vel.x));
+            ctxGeo.lineTo(0, 0);
+            ctxGeo.lineWidth = 0.02;
+            ctxGeo.strokeStyle = "#ff0";
+            ctxGeo.stroke();
+            ctxGeo.beginPath();
+            ctxGeo.lineTo(-0.1, 0.1);
+            ctxGeo.lineTo(-0.1, -0.1);
+            ctxGeo.lineTo(0, 0);
+            ctxGeo.closePath(); 
+            ctxGeo.stroke();
+            ctxGeo.restore();
+        }
+    }
+
+    if (obs.wav !== null) {
+        if (Math.hypot(ovel.x, ovel.y) > 0.01 * wspd) {
+            ctxGeo.save();
+            ctxGeo.translate(obs.pos.x, obs.pos.y);
+            ctxGeo.beginPath();
+            ctxGeo.lineTo(0, 0);
+            ctxGeo.translate(ovel.x / wspd, ovel.y / wspd);
+            ctxGeo.rotate(Math.atan2(ovel.y, ovel.x));
+            ctxGeo.lineTo(0, 0);
+            ctxGeo.lineWidth = 0.02;
+            ctxGeo.strokeStyle = "#080";
+            ctxGeo.stroke();
+            ctxGeo.beginPath();
+            ctxGeo.lineTo(-0.1, 0.1);
+            ctxGeo.lineTo(-0.1, -0.1);
+            ctxGeo.lineTo(0, 0);
+            ctxGeo.closePath(); 
+            ctxGeo.stroke();
+            ctxGeo.restore();
+        }
+
+        if (Math.hypot(svel.x, svel.y) > 0.01 * wspd) {
+            ctxGeo.save();
+            ctxGeo.translate(obs.wav.pos.x, obs.wav.pos.y);
+            ctxGeo.beginPath();
+            ctxGeo.lineTo(0, 0);
+            ctxGeo.translate(svel.x / wspd, svel.y / wspd);
+            ctxGeo.rotate(Math.atan2(svel.y, svel.x));
+            ctxGeo.lineTo(0, 0);
+            ctxGeo.lineWidth = 0.02;
+            ctxGeo.strokeStyle = "#880";
+            ctxGeo.stroke();
+            ctxGeo.beginPath();
+            ctxGeo.lineTo(-0.1, 0.1);
+            ctxGeo.lineTo(-0.1, -0.1);
+            ctxGeo.lineTo(0, 0);
+            ctxGeo.closePath(); 
+            ctxGeo.stroke();
+            ctxGeo.restore();
+        }
     }
 
     ctxGeo.restore();
